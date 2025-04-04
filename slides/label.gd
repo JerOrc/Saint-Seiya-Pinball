fa83bc0c80ad81bed41da9ddf7cc2cb9b30d2aae
JerOrc <jvspams-github@yahoo.fr>
2025-04-04 15:53:52 +0200

    switches and attract carousel

--- slides/label.gd
+++ slides/label.gd
@@ -1,17 +1,17 @@
 extends Label
 
-var exemple_text = "Eurêka !"
+var exemple_text = "Saint Seiya !"
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	scroll_text(exemple_text) 
+  scroll_text(exemple_text) 
 
 func scroll_text(input_text:String) -> void:
-	visible_characters = 0
-	text = input_text	
-	var i = 0
-	while true:
-		i = (i+1)%10
-		visible_characters = i
-		await get_tree().create_timer(0.2).timeout
-		
+  visible_characters = 0
+  text = input_text	
+  var i = 0
+  while true:
+    i = (i+1)%(input_text.length()+1)
+    visible_characters = i
+    await get_tree().create_timer(0.2).timeout
+    
