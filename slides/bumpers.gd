756c2ad67ebbc35c9bd33cf6041d09f2d4e10944
JerOrc <JerOrc@users.noreply.github.com>
2025-08-19 14:52:01 +0200

    Use of Godot scene duplication

--- slides/bumpers.gd
+++ slides/bumpers.gd
@@ -1,9 +1,10 @@
 extends MPFSlide
 
+var scene
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-  pass # Replace with function body.
+  scene = preload("res://widgets/bumpers.tscn")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -12,4 +13,7 @@ func _process(delta: float) -> void:
 
 
 func top_down_text(_settings, _kwargs):
-  $zedmd/AnimationPlayer.play("top_down")
+  var instance = scene.instantiate()
+  # instance.position = Vector2("50,0")
+  add_child (instance)
+  #$zedmd/AnimationPlayer.play("top_down")
