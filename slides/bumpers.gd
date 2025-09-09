f7ddca47f48ba4f6e2e20dfab5e80db471e4624a
JerOrc <jvspams-github@yahoo.fr>
2025-09-09 16:42:54 +0200

    Use scenes instead of  widgets

--- slides/bumpers.gd
+++ slides/bumpers.gd
@@ -4,7 +4,7 @@ var scene
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-  scene = preload("res://widgets/bumpers.tscn")
+  scene = preload("res://scenes/bumpers.tscn")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
--- slides/log.gd
+++ slides/log.gd
@@ -1,7 +1,22 @@
 extends MPFSlide
 
+
+var scene
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+  scene = preload("res://scenes/log.tscn")
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+  pass
+
+
 func log_text(_settings, _kwargs):
-  $zedmd/Label.text = _settings.tokens.text
-  $zedmd.visible = true
+  # print("player score : " + $zedmd/MPFVariable.text)
+  var instance = scene.instantiate()
+  instance.get_node("Label").text = _settings.tokens.text
+  $zedmd.add_child(instance)
   await get_tree().create_timer(1).timeout
-  $zedmd.visible = false
+  instance.queue_free()
